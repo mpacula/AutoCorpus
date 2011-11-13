f2e399d9b17b5403545e5571bb4e1dd591ac6f81
Maciej Pacula <maciej.pacula@gmail.com>
2011-11-12 20:34:27 -0500

    remove reduntant files under debian/

--- debian/autocorpus.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the autocorpus package
-#
-0 4	* * *	root	[ -x /usr/bin/autocorpus_maintenance ] && /usr/bin/autocorpus_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(autocorpus):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="autocorpus" command="/usr/bin/autocorpus"
